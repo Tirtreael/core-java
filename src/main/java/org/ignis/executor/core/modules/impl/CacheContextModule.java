e430a56819ef6ebbf621bf6d879eb78098079036
Tirtreael <saephirenn@gmail.com>
2023-01-24 17:06:53 +0100

    Created CacheContextModule and IZlibTransportFactory. Removed .idea

---
+++ src/main/java/org/ignis/executor/core/modules/impl/CacheContextModule.java
@@ -0,0 +1,6 @@
+package org.ignis.executor.core.modules.impl;
+
+import org.ignis.executor.core.modules.ICacheContextModule;
+
+public class CacheContextModule implements ICacheContextModule {
+}
---
+++ src/main/java/org/ignis/executor/core/transport/IZlibTransportFactory.java
@@ -0,0 +1,18 @@
+package org.ignis.executor.core.transport;
+
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
+import org.apache.thrift.transport.TZlibTransport;
+
+public class IZlibTransportFactory extends TZlibTransport.Factory {
+
+    private int compression;
+    public IZlibTransportFactory(int compression) {
+        this.compression = compression;
+    }
+
+    @Override
+    public TTransport getTransport(TTransport base) throws TTransportException {
+        return new TZlibTransport(base, compression);
+    }
+}
